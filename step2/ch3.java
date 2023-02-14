5ec2b5f5494249296de053cf93c59f8fa8e361d0
seoa <yoona103111@namer.com>
2023-02-14 20:08:07 +0900

    baekjoon

--- step2/ch3.java
+++ step2/ch3.java
@@ -1,7 +1,17 @@
 package doit.one.basic.step2;
 
+import java.util.Scanner;
+/*연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.*/
+/*2753번*/
 public class ch3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        sc.close();
+        if((A%4==0&&A%100!=0)||A%400==0){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
     }
 }
